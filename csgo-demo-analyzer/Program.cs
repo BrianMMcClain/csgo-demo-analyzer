1642c8af9ce12c1fd2d18fc239096077213f9dec
Brian McClain <brianmmcclain@gmail.com>
2015-05-30 20:27:47 -0400

    Initiaal commit, experimental work

---
+++ csgo-demo-analyzer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csgo_demo_analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoAnalyzerWorker worker = new DemoAnalyzerWorker("D:\\demos\\test_demo.dem");
+        }
+    }
+}
---
+++ csgo-demo-analyzer/lib/DemoAnalyzerWorker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DemoInfo;
+
+namespace csgo_demo_analyzer
+{
+    class DemoAnalyzerWorker
+    {
+        public DemoAnalyzerWorker(String demoPath)
+        {
+            DemoParser parser = new DemoParser(File.OpenRead(demoPath));
+            parser.ParseHeader();
+
+            Debug.WriteLine("Map: " + parser.Map);
+        }
+    }
+}
