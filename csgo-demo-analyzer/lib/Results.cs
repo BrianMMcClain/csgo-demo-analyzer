4c3a7f007cb161b3c6ec8d156ab7a31e31622ce0
Brian McClain <brianmmcclain@gmail.com>
2015-06-05 00:58:06 -0400

    Added ESEA-like result categories (Most Deadly, Mostly Harmless, etc)

--- csgo-demo-analyzer/lib/Results.cs
+++ csgo-demo-analyzer/lib/Results.cs
@@ -17,20 +17,98 @@ namespace csgo_demo_analyzer
         {
             get
 			{
-				Player mostHS = null;
-				int mostHSCount = -1;
-				foreach (Player p in this.Players.Values)
-				{
-					if (p.HeadshotCount > mostHSCount)
-					{
-						mostHS = p;
-						mostHSCount = p.HeadshotCount;
-					}
-				}
-
-				return mostHS;
-			}
+                return this.Players.Aggregate((a, b) => a.Value.HeadshotCount > b.Value.HeadshotCount ? a : b).Value;
+            }
         }
+
+        // ESEA: Most Deadly
+        public Player MostKills
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Kills.Count > b.Value.Kills.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Mostly Harmless
+        public Player LeastKills
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Kills.Count < b.Value.Kills.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Most Professional
+        public Player HighestFragDeathRatio
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => ((float)a.Value.Kills.Count / (float)a.Value.Deaths.Count) > ((float)b.Value.Kills.Count / (float)b.Value.Deaths.Count) ? a : b).Value;
+            }
+        }
+
+        // ESEA: Longest Innings
+        public Player LeastDeaths
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Deaths.Count < b.Value.Deaths.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Shortest Innings
+        public Player MostDeaths
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Deaths.Count > b.Value.Deaths.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Most Honorable
+        public Player LeastTKs
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Teamkills.Count < b.Value.Teamkills.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Most Dishonorable
+        public Player MostTKs
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.Teamkills.Count > b.Value.Teamkills.Count ? a : b).Value;
+            }
+        }
+
+        // ESEA: Most Cowardly
+        public Player MostCowardly
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => (a.Value.Kills.Count + a.Value.Deaths.Count) < (b.Value.Kills.Count + b.Value.Deaths.Count) ? a : b).Value;
+            }
+        }
+
+        public Player MostBombPlants
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.BombPlants.Count > b.Value.BombPlants.Count ? a : b).Value;
+            }
+        }
+
+        public Player MostBombDefuses
+        {
+            get
+            {
+                return this.Players.Aggregate((a, b) => a.Value.BombDefuses.Count > b.Value.BombDefuses.Count ? a : b).Value;
+            }
+        }
+
 
         public Results()
         {
